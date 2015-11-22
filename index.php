592ec22d746a47cb9b3655cf56d44151ceb1c3ad
qingran001 <qingran001@gmail.com>
2015-11-22 20:24:21 +0800

    d

---
+++ index.php
@@ -0,0 +1,69 @@
+﻿<?php
+include_once 'lib/config.php';
+include_once 'header.php';
+?>
+
+
+<div class="section no-pad-bot" id="index-banner">
+        <div class="container">
+            <br><br>
+            <h1 class="header center orange-text">milu-shadowsocks</h1>
+            <div class="row center">
+                <h5 class="header col s12 light">麋鹿路由科学上网   定制保护个人隐私</h5>
+            </div>
+            <div class="row center">
+                <a href="user/register.php" id="download-button" class="btn-large waves-effect waves-light orange">立即注册</a></a></a>
+            </div>
+            <br><br>
+        </div>
+</div>
+
+
+<div class="container">
+    <div class="section">
+
+        <!--   Icon Section   -->
+        <div class="row">
+            <div class="col s12 m4">
+                <div class="icon-block">
+                    <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
+                    <h5 class="center">迅捷连接</h5>
+
+                    <p class="light">
+                        您只需要输入帐号密码，一切交给我们来做,麋鹿路由用户已提前预置shadowsocks，一键开启智享互联.
+                    </p>
+                </div>
+            </div>
+
+            <div class="col s12 m4">
+                <div class="icon-block">
+                    <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
+                    <h5 class="center">用户至上</h5>
+
+                    <p class="light">
+                        7x24小时上门服务，根据用户习惯定义您独一无二的智能路由，路由器智能切换，国内走国内流量，0延迟更畅快
+                    </p>
+                </div>
+            </div>
+
+            <div class="col s12 m4">
+                <div class="icon-block">
+                    <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
+                    <h5 class="center">固件更新</h5>
+
+                    <p class="light">
+                        支持多客户端使用（Android and iOS) and Routers (OpenWRT），固件可升级，服务有保证
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <br><br>
+
+    <div class="section">
+
+    </div>
+</div>
+<?php  include_once 'ana.php';
+       include_once 'footer.php';?>
